870c0e2b48bfb41757857b32da7de124ecc5692e
Paolo D'Incau <paolo.dincau@gmail.com>
2017-01-25 16:52:36 +0100

    add route to handle post request

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,11 @@
 import com.spotify.apollo.Environment;
+import com.spotify.apollo.RequestContext;
+import com.spotify.apollo.Response;
 import com.spotify.apollo.httpservice.HttpService;
 import com.spotify.apollo.httpservice.LoadingException;
 import com.spotify.apollo.route.Route;
+
+import static com.spotify.apollo.Status.CREATED;
 
 public class Application {
 
@@ -11,7 +15,12 @@ public class Application {
 
     static void init(Environment environment) {
         environment.routingEngine()
+                .registerAutoRoute(Route.sync("POST", "/people", Application::addPerson))
                 .registerAutoRoute(Route.sync("GET", "/", rc -> "Yet another microservice from XPeppers"));
+    }
+
+    private static Response addPerson(RequestContext context) {
+        return Response.forStatus(CREATED);
     }
 
 }
